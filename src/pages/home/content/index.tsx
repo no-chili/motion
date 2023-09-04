78bf275ddfb65cbaefdae03af33f22d8f7e4dcb2
no-chili <18875854140@163.com>
2023-09-04 20:24:08 +0800

    vercel.json

--- src/pages/home/content/index.tsx
+++ src/pages/home/content/index.tsx
@@ -24,7 +24,7 @@ export default function Content() {
 				给个机会
 			</h1>
 			<LayoutGroup>
-				<motion.div className='md:m-5 mt-10 lg:mx-0 lg:mt-30'>
+				<motion.div className='m-5 mt-10 lg:mx-0 lg:mt-30'>
 					<CodeEdit />
 				</motion.div>
 				<motion.div className='flex m-5 lg:mx-0 flex-wrap sm:my-5 justify-center md:justify-between'>
